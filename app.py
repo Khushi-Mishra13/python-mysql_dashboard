852ae309f09a351208a57461c67a5085c1255224
Khushi-Mishra13 <mishrakhushi1310@gmail.com>
2026-09-08 14:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_db ():
 			database = os.getenv("MYSQL_DATABASE", "dashboard")
 			)
 
-@app.after_request():
+@app.after_request()
 def add_security_headers(response):
 	response.headers["Content-Security-Policy"] = (
         "default-src 'self'; "
