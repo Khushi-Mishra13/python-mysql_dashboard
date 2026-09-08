29f77156373c9304d6e66380d2514cb48a578a6c
Khushi-Mishra13 <mishrakhushi1310@gmail.com>
2026-09-08 14:41:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ def get_db ():
 			password = os.getenv("MYSQL_PASSWORD", "password"),
 			database = os.getenv("MYSQL_DATABASE", "dashboard")
 			)
+
+@app.after_request():
+def add_security_headers(response):
+	response.headers["Content-Security-Policy"] = (
+        "default-src 'self'; "
+        "script-src 'self'; "
+        "style-src 'self'; "
+        "img-src 'self' data:; "
+        "font-src 'self'; "
+        "object-src 'none'; "
+        "frame-ancestors 'none'; "
+        "base-uri 'self';"
+    )
+    return response
 @app.route("/")
 def home ():
 	
