dc925b669299f234d0072fc9a177968aa2c4f9ef
Khushi-Mishra13 <mishrakhushi1310@gmail.com>
2026-09-08 14:55:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_db ():
 
 @app.after_request
 def add_security_headers(response):
-	response.headers["Content-Security-Policy"] = (
+    response.headers["Content-Security-Policy"] = (
         "default-src 'self'; "
         "script-src 'self'; "
         "style-src 'self'; "
